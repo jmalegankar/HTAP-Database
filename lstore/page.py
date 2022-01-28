2ac0f1df598bd02ef288b0cd5f7e56edec32db05
patastic4 <patchan36@yahoo.com>
2022-01-27 18:08:30 -0800

    added some more comments if anyone is interested

--- lstore/page.py
+++ lstore/page.py
@@ -18,6 +18,11 @@
 
 #How many pages should we have per page range?
 
+#For now now implment bytearray for ints IDK if we need strings as tester1 does not use it
+
+# I am assuming we are allowed to use our own imports lol
+# import struct
+
 class Page:
 
     def __init__(self):
@@ -27,7 +32,39 @@ class Page:
     def has_capacity(self):
         pass
 
+    # Patrick
+    # Possible need for future implmentation
+    # This stores int values greater than 255. as bytearray
+    # can only store 0 < x < 256 integer in an index
+    # I am assuming we do no need to store ints bigger than this 
+    # so for now we only need the "else" part tbh
+    # need a differnet implmentation for strings maybe 
+    # have not looked into it
+
+    # The question: When storing values shouls we use a fixed length or
+    # a variable length? 
+    # because the student ID given is 92106429 + 1000 in the tester which
+    # at max is a 32 bit int
+
+    #def write(self, value):
+    #    if value > 255:
+    #        value=(self.intTobit(value)).lstrip(b'\x00')
+    #        for eightBit in value:
+    #            self.data[self.num_records]=eightBit
+    #            self.num_records += 1
+    #    else:
+    #       self.data[self.num_records]=value
+    #       self.num_records += 1 
+    #    pass
+
+
     def write(self, value):
+        self.data[self.num_records]=value
         self.num_records += 1
         pass
+
+    # turns number into a string of bits for function commented out above
+    #def intTobit(intNumber):
+    #    byteSTR = struct.pack('>Q',intNumber)
+    #    return byteSTR
 
