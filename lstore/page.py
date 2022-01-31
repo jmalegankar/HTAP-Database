ac42da38d8bdc56d992cd0a305addad8b00a2aa4
guan-kevin <github@pigzy.net>
2022-01-30 18:34:44 -0800

    Improved page, used signed int64

--- lstore/page.py
+++ lstore/page.py
@@ -21,45 +21,26 @@
 #For now now implment bytearray for ints IDK if we need strings as tester1 does not use it
 
 # I am assuming we are allowed to use our own imports lol
-import struct
-
 class Page:
 
     def __init__(self):
         self.num_records = 0
         self.data = bytearray(4096)
-        self.cur_records = 0
 
     def has_capacity(self):
-        return (self.cur_records < 512)
+        return self.num_records < 512
 
     # writes a record of an int up to size of 64 bits, ALL records will take up 8 spaces
     # in this implmentation we can switch to a variable length storage if we want but for now this was
     # easiest to do
     def write(self, value):
-        if value > 255:
-            value=self.intTobit(value)
-            for eightBit in value:
-                self.data[self.cur_records]=eightBit
-                self.cur_records += 1
-        else:
-           self.data[self.cur_records+7]=value
-           self.cur_records += 8
+        assert self.has_capacity()
+        self.data[self.num_records * 8 : self.num_records * 8 + 8] = \
+        value.to_bytes(8, 'big', signed=True)
         self.num_records += 1
-        pass
 
-
-    #turns number into a string of bits for function commented out above
-    def intTobit(self, intNumber):
-        byteSTR = struct.pack('>Q',intNumber)
-        return byteSTR
-
-    # pass in the rec number we want. so rec 0 - 512
+    # pass in the rec number we want. so rec 0 ..< 512
     # can aslso use rec offset which will probably be better tbh
     def get(self, rec_num):
-        buffer1=bytearray(8)
-        buffer1 = self.data[rec_num*8: (rec_num+1)*8]
-        return(int.from_bytes(buffer1, byteorder='big', signed=False))
-        pass
-    
-
+        assert 0 <= rec_num < 512
+        return int.from_bytes(self.data[rec_num * 8 : rec_num * 8 + 8], "big", signed=True)
