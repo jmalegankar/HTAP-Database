e57173b96cbfc01442f37d29da2c24831c948936
JMalegaonkar <jmalegaonkar2002@gmail.com>
2022-02-02 11:53:37 -0800

    update tail page and base page class

--- lstore/basepage.py
+++ lstore/basepage.py
@@ -1,2 +1,16 @@
+from lstore.page import Page
+
+
 class BasePage:
-	
+	def __init__(self, columns:int):
+        self.num_records = 0
+        phys_pages = [Page()] * columns
+    def get(self):
+    	pass
+    def set(self):
+    	pass
+    def update(self):
+    	pass
+
+
+        
--- lstore/tailpage.py
+++ lstore/tailpage.py
@@ -1,1 +1,12 @@
+from lstore.page import Page
+
 class TailPage:
+	def __init__(self, columns:int):
+        self.num_records = 0
+        phys_pages = [Page()] * columns
+    def get(self):
+    	pass
+    def set(self):
+    	pass
+    def update(self):
+    	pass
