56af83f1734ba094e7a6cef5c9eaadc013e3ae38
Sonya Gomez Enriquez <s.gomezxd@gmail.com>
2022-02-06 16:41:31 -0800

    Update btree.py

--- BTree/btree.py
+++ BTree/btree.py
@@ -1,19 +1,17 @@
+class BTreeNode():
+  pass
+class InternalNode(BTreeNode):
+  pass
+class LeafNode(BTreeNode):
+  pass
+
 class BTreeNode:
-  def __init__(self, lSize = None, p = None, l = None, r = None):
+  def __init__(self, lSize = 0 , p = InternalNode(), l = BTreeNode(), r = BTreeNode()):
     self.count = 0
     self.leafSize = lSize
-    if p is None:
-      self.parent = InternalNode()
-    else:
-      self.parent = p
-    if l is None:
-      self.leftSibling = BTreeNode()
-    else:
-      self.leftSibling = l
-    if r is None:
-      self.rightSibling = BTreeNode()
-    else:
-      self.rightSibling = r
+    self.parent = p
+    self.leftSibling = l
+    self.rightSibling = r
   
   # set parent
   def setParent(self, x):
@@ -28,189 +26,206 @@ class BTreeNode:
     self.leftSibling = x
 
 class InternalNode(BTreeNode):
-  def __init__(self, iSize = None, lSize = None, p = None, l = None, r = None):
+  def __init__(self, iSize = 0, lSize = 0, p = InternalNode(), l = BTreeNode(), r = BTreeNode()):
+    BTreeNode.__init__(self, lSize, p, l , r)
     self.internalSize = iSize
     self.keys = []
     self.children = []
-    def addPtr(self, ptr: BTreeNode, pos: int):
-        if (pos == self.internalSize):
-            return ptr
-        last = BTreeNode(self.children[self.count - 1])
-    
-        for i in range(self.count - 2, pos, -1):
-            self.children[i + 1] = self.children[i]
-            self.keys[i + 1] = self.keys[i]
-        
-        self.children[pos] = ptr
-        self.keys[pos] = ptr.getMinimum()
-        ptr.setParent(self)
-        return last
-    def addToLeft(self, last: BTreeNode):
-        self.leftSibling.insertNewNode(self.children[0])
-        for i in range(0, self.count-1):
-            self.children[i] = self.children[i+1]
-            self.keys[i] = self.keys[i+1]
-        self.children[self.count -1] = last
-        self.keys[self.count-1] = last.getMinimum() 
-        last.setParent(self)
-        if (self.parent):
-            self.parent.resetMinimum(self)
-    def addToRight(self, ptr: BTreeNode, last:BTreeNode):
-        self.rightSibling.insertNewNode(last)
-        if(ptr == self.children[0] and self.parent):
-            self.parent.resetMinimum(self)
-    def addToThis(self, ptr: BTreeNode, pos: int):
-        for i in range(self.count-1, pos, -1):
-            self.children[i+1] = self.children[i]
-            self.keys[i+1] = self.keys[i]
-        self.children[pos] = ptr
-        self.keys[pos] = ptr.getMinimum()
-        self.count += 1
-        ptr.setParent(self)
-        if (pos==0 and self.parent):
-            self.parent.resetMinimum(self)
-    def getMaximum(self): 
-        if (self.count > 0):
-            return self.children[self.count-1].getMaximum()
-    def getMinimum(self):
-        if (self.count > 0):
-            return self.children[0].getMinimum()
-    def insertValue(self, value): #there are 3 insert functions???
-      pos = 0
-      if self.keys[pos] > value:
-          for pos in range(self.count - 1, 0, -1):
-              if self.keys[pos] > value:
-                  continue
-              else:
-                  break
-      last = BTreeNode(self.children[pos].insertValue(value)) 
-      ptr =  BTreeNode(self.children[pos].insertValue(value))    
-       
-      if not ptr :
-          pass
-      if self.count < self.internalSize:
-          self.addToThis(ptr, pos + 1)
-          pass
-      
-      last = self.addPtr(ptr, pos + 1)
-      if self.leftSibling and self.leftSibling.count < self.internalSize:
-          self.addToLeft(last)
-          pass
-      elif self.rightSibling and self.rightSibling.count < self.internalSize:
-          self.addToRight(ptr, last)
-          pass
-      else:
-          return self.split(last)
-    def insertOldRoot(self, oldRoot, node2):
-      self.children[0] = oldRoot
-      self.children[1] = node2
-      self.keys[0] = oldRoot.getMinimum()
-      self.keys[1] = node2.getMinimum()
-      self.bTNode.count = 2
-      self.children[0].setLeftSibling(None)
-      self.children[0].setRightSibling(self.children[1])
-      self.children[1].setLeftSibling(self.children[0])
-      self.children[1].setRightSibling(None)
-      oldRoot.setParent(self)
-      node2.setParent(self)
-    def insertNewNode(self, newNode):
-      pos = 0
-      if newNode.getMinimum() <= self.keys[0]:
-          pos = 0
-      else:
-          pos = self.count
+
+  def addPtr(self, ptr: BTreeNode, pos: int):
+      if (pos == self.internalSize):
+          return ptr
+      last = BTreeNode(self.children[self.count - 1])
   
-      self.addToThis(newNode, pos)
-    def remove(self, value: int):
-        pos, i = 0
-        for pos in range(self.count-1, 0, -1):
+      for i in range(self.count - 2, pos, -1):
+          self.children[i + 1] = self.children[i]
+          self.keys[i + 1] = self.keys[i]
+      
+      self.children[pos] = ptr
+      self.keys[pos] = ptr.getMinimum()
+      ptr.setParent(self)
+      return last
+
+  def addToLeft(self, last: BTreeNode):
+      self.leftSibling.insertNewNode(self.children[0])
+      for i in range(0, self.count-1):
+          self.children[i] = self.children[i+1]
+          self.keys[i] = self.keys[i+1]
+      self.children[self.count -1] = last
+      self.keys[self.count-1] = last.getMinimum() 
+      last.setParent(self)
+      if (self.parent):
+          self.parent.resetMinimum(self)
+
+  def addToRight(self, ptr: BTreeNode, last:BTreeNode):
+      self.rightSibling.insertNewNode(last)
+      if(ptr == self.children[0] and self.parent):
+          self.parent.resetMinimum(self)
+
+  def addToThis(self, ptr: BTreeNode, pos: int):
+      for i in range(self.count-1, pos, -1):
+          self.children[i+1] = self.children[i]
+          self.keys[i+1] = self.keys[i]
+      self.children[pos] = ptr
+      self.keys[pos] = ptr.getMinimum()
+      self.count += 1
+      ptr.setParent(self)
+      if (pos==0 and self.parent):
+          self.parent.resetMinimum(self)
+
+  def getMaximum(self): 
+      if (self.count > 0):
+          return self.children[self.count-1].getMaximum()
+
+  def getMinimum(self):
+      if (self.count > 0):
+          return self.children[0].getMinimum()
+
+  def insertValue(self, value): #there are 3 insert functions???
+    pos = 0
+    if self.keys[pos] > value:
+        for pos in range(self.count - 1, 0, -1):
             if self.keys[pos] > value:
                 continue
             else:
                 break
-        ptr = BTreeNode(self.children[pos].remove(value))
-        if ptr:
-            ptr = None
-            self.count -= 1
-            for i in range(pos, self.count):
-                self.children[i] = self.children[i+1]
-                self.keys[i] = self.keys[i+1]
-            if (self.count < ((self.internalSize +1)/2) or self.count == 1):
-                if (self.leftSibling):
-                    return self.removeWithLeftSibling()
-                elif (self.rightSibling):
-                    return self.removeWithRightSibling(pos)
-                
-            if (self.parent and pos ==0 and self.count > 0):
-                self.parent.resetMinimum(self)
-        if (self.count == 1 and self.parent == None):
-            return self.children[0]
+    last = BTreeNode(self.children[pos].insertValue(value)) 
+    ptr =  BTreeNode(self.children[pos].insertValue(value))    
+      
+    if not ptr :
         pass
-    def removeChild(self, position: int):
-        ptr = BTreeNode(self.children[position]) 
-        self.count -= 1
-        for i in range (position, self.count):
-            self.children[i] = self.children[i+1]
-            self.keys[i] = self.keys[i+1]
-        if (position == 0 and self.parent):
-            self.parent.resetMinimum(self)
-        return ptr
-    def removeWithLeftSibling(self):
-        if (self.leftSibling.count > ((self.internalSize + 1)/2) ):
-            self.insertNewNode(self.leftSibling.removeChild(self.leftSibling.count - 1) )
-            if (self.parent):
-                self.parent.resetMinimum(self)
-            pass
-        else:
-            for i in range(0, self.count):
-                self.leftSibling.insertNewNode(self.children[i])
-            self.leftSibling.setRightSibling(self.rightSibling)
-            if (self.rightSibling):
-                self.rightSibling.setLeftSibling(self.leftSibling)
-            return self
-    def removeWithRightSibling(self, position: int):
-        if (self.rightSibling.count > (self.internalSize + 1)/2):
-            self.insertNewNode(self.rightSibling.removeChild(0))
-            if (position == 0):
-                self.parent.resetMinimum(self)
-            pass
-        else:
-            for i in range(self.count-1, 0, -1):
-                self.rightSibling.insertNewNode(self.children[i])
-            self.rightSibling.setLeftSibling(self.leftSibling)
-            if (self.leftSibling):
-                self.leftSibling.setRightSibling(self.rightSibling)
-            return self 
-    def resetMinimum(self, child: BTreeNode):
-        for i in range (0, self.count):
-            if self.children[i] == child:
-                self.keys[i] = self.children[i].getMinimum()
-                if (i == 0 and self.parent):
-                    self.parent.resetMinimum(self)
-            break
-    def split(self, last: BTreeNode):
-        newPtr = InternalNode(self.internalSize, self.leafSize, self.parent, self, self.rightSibling)
-        if self.rightSibling:
-            self.rightSibling.setLeftSibling(newPtr)
-                    
-        self.rightSibling = newPtr
-                    
-        for i in range( (self.internalSize + 1) / 2, self.internalSize ):
-            newPtr.children[newPtr.count] = self.children[i] 
-            newPtr.keys[newPtr.count] = self.keys[i]
-            newPtr.count += 1
-            self.children[i].setParent(newPtr) 
-        newPtr.children[newPtr.count] = last
-        newPtr.keys[newPtr.count] = last.getMinimum()
-        newPtr.count += 1
-        last.setParent(newPtr)
-        self.count = (self.internalSize + 1) /2
-        return newPtr
+    if self.count < self.internalSize:
+        self.addToThis(ptr, pos + 1)
+        pass
+    
+    last = self.addPtr(ptr, pos + 1)
+    if self.leftSibling and self.leftSibling.count < self.internalSize:
+        self.addToLeft(last)
+        pass
+    elif self.rightSibling and self.rightSibling.count < self.internalSize:
+        self.addToRight(ptr, last)
+        pass
+    else:
+        return self.split(last)
+
+  def insertOldRoot(self, oldRoot, node2):
+    self.children[0] = oldRoot
+    self.children[1] = node2
+    self.keys[0] = oldRoot.getMinimum()
+    self.keys[1] = node2.getMinimum()
+    self.bTNode.count = 2
+    self.children[0].setLeftSibling(None)
+    self.children[0].setRightSibling(self.children[1])
+    self.children[1].setLeftSibling(self.children[0])
+    self.children[1].setRightSibling(None)
+    oldRoot.setParent(self)
+    node2.setParent(self)
+
+  def insertNewNode(self, newNode):
+    pos = 0
+    if newNode.getMinimum() <= self.keys[0]:
+        pos = 0
+    else:
+        pos = self.count
+
+    self.addToThis(newNode, pos)
+
+  def remove(self, value: int):
+      pos, i = 0
+      for pos in range(self.count-1, 0, -1):
+          if self.keys[pos] > value:
+              continue
+          else:
+              break
+      ptr = BTreeNode(self.children[pos].remove(value))
+      if ptr:
+          ptr = None
+          self.count -= 1
+          for i in range(pos, self.count):
+              self.children[i] = self.children[i+1]
+              self.keys[i] = self.keys[i+1]
+          if (self.count < ((self.internalSize +1)/2) or self.count == 1):
+              if (self.leftSibling):
+                  return self.removeWithLeftSibling()
+              elif (self.rightSibling):
+                  return self.removeWithRightSibling(pos)
+              
+          if (self.parent and pos ==0 and self.count > 0):
+              self.parent.resetMinimum(self)
+      if (self.count == 1 and self.parent == None):
+          return self.children[0]
+      pass
+
+  def removeChild(self, position: int):
+      ptr = BTreeNode(self.children[position]) 
+      self.count -= 1
+      for i in range (position, self.count):
+          self.children[i] = self.children[i+1]
+          self.keys[i] = self.keys[i+1]
+      if (position == 0 and self.parent):
+          self.parent.resetMinimum(self)
+      return ptr
+
+  def removeWithLeftSibling(self):
+      if (self.leftSibling.count > ((self.internalSize + 1)/2) ):
+          self.insertNewNode(self.leftSibling.removeChild(self.leftSibling.count - 1) )
+          if (self.parent):
+              self.parent.resetMinimum(self)
+          pass
+      else:
+          for i in range(0, self.count):
+              self.leftSibling.insertNewNode(self.children[i])
+          self.leftSibling.setRightSibling(self.rightSibling)
+          if (self.rightSibling):
+              self.rightSibling.setLeftSibling(self.leftSibling)
+          return self
+
+  def removeWithRightSibling(self, position: int):
+      if (self.rightSibling.count > (self.internalSize + 1)/2):
+          self.insertNewNode(self.rightSibling.removeChild(0))
+          if (position == 0):
+              self.parent.resetMinimum(self)
+          pass
+      else:
+          for i in range(self.count-1, 0, -1):
+              self.rightSibling.insertNewNode(self.children[i])
+          self.rightSibling.setLeftSibling(self.leftSibling)
+          if (self.leftSibling):
+              self.leftSibling.setRightSibling(self.rightSibling)
+          return self 
+
+  def resetMinimum(self, child):
+      for i in range (0, self.count):
+          if self.children[i] == child:
+              self.keys[i] = self.children[i].getMinimum()
+              if (i == 0 and self.parent):
+                  self.parent.resetMinimum(self)
+          break
+
+  def split(self, last: BTreeNode):
+      newPtr = InternalNode(self.internalSize, self.leafSize, self.parent, self, self.rightSibling)
+      if self.rightSibling:
+          self.rightSibling.setLeftSibling(newPtr)
+                  
+      self.rightSibling = newPtr
+                  
+      for i in range( (self.internalSize + 1) / 2, self.internalSize ):
+          newPtr.children[newPtr.count] = self.children[i] 
+          newPtr.keys[newPtr.count] = self.keys[i]
+          newPtr.count += 1
+          self.children[i].setParent(newPtr) 
+      newPtr.children[newPtr.count] = last
+      newPtr.keys[newPtr.count] = last.getMinimum()
+      newPtr.count += 1
+      last.setParent(newPtr)
+      self.count = (self.internalSize + 1) /2
+      return newPtr
         
 
 class LeafNode(BTreeNode):
-  def __init__(self, lSize, p = None, l = None, r = None):
-    self.values = []
+  def __init__(self, lSize, p = InternalNode(), l = BTreeNode(), r = BTreeNode()):
+    BTreeNode.__init__(self, lSize, p, l , r)
+    self.values = [None] * 5000
     
   def addToLeft(self, value, last):
     self.leftSibling.insertValue(value[0])
@@ -226,14 +241,17 @@ class LeafNode(BTreeNode):
         self.parent.resetMinimum(self.parent)
       
   def addToThis(self, value):
-    for i in range(self.count - 1, 0 , -1 ): 
-        if self.values[i] > value:
-            self.values[i+1] = self.values[i]
+    i = self.count - 1
 
-    self.values[i+1] = value;
+    if i >= 0 and self.values[i] > value:
+      for i in range(self.count - 1, 0 , -1 ): 
+         if i >= 0 and self.values[i] > value:
+          self.values[i+1] = self.values[i]
+    self.values[i+1] = value
+
     self.count = self.count + 1
     if (value == self.values[0] and self.parent):
-        self.parent.resetMinimum(self.parent)
+      (self.parent).resetMinimum(self.parent)
 
   def addValue(self, value, last):
     if value > self.values[self.count-1]:
@@ -297,7 +315,7 @@ class LeafNode(BTreeNode):
         self.insertValue(self.leftSibling.getMaximum())
         self.leftSibling.remove(self.values[0]) #not sure 
     if (self.parent):
-        self.bTNode.parent.resetMinimum(self.bTNode.parent)
+        self.parent.resetMinimum(self.parent)
         return None
     else:
         for i in range(0, self.count):
@@ -326,7 +344,7 @@ class LeafNode(BTreeNode):
     newPtr =  LeafNode(self.leafsize, self.parent, self, self.rightSibling)
   
     if (self.rightSibling):
-        self.rightSibling = self.bTNode.setLeftSibling(newPtr)
+        self.rightSibling = self.setLeftSibling(newPtr)
     self.rightSibling = newPtr
   
     for i in range(((self.leafSize +1)/2), self.leafSize):
