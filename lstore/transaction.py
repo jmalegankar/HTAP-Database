97c278b9e413e65aad1fd0f44636de36e0438862
guan-kevin <github@pigzy.net>
2022-03-08 22:12:59 -0800

    Added index log Abort WIP
    Co-authored-by: patastic4 <patastic4@users.noreply.github.com>

--- lstore/transaction.py
+++ lstore/transaction.py
@@ -8,7 +8,7 @@ import lstore.bufferpool as bufferpool
 
 class Transaction:
 
-    __slots__ = 'queries', 'tid', 'success_rids', 'locks', 'update_rid', 'key_locks'
+    __slots__ = 'queries', 'tid', 'success_rids', 'locks', 'update_rid', 'query_index', 'tables'
 
     """
     # Creates a transaction object.
@@ -23,8 +23,13 @@ class Transaction:
         self.success_rids = []
         # for unlock_all_locks
         self.locks = []
-        self.key_locks = []
+
         self.update_rid = {} # map rid 
+
+        self.query_index = {} # used to record original record
+
+        self.tables = {}
+
 
     def __str__(self):
         string = 'Transaction {}\n'.format(self.tid)
@@ -35,8 +40,10 @@ class Transaction:
             string += '{}: {}\n'.format(query.__name__, args)
         return string
 
+
     def __repr__(self):
         return self.__str__()
+
 
     """
     # Adds the given query to this transaction
@@ -45,55 +52,86 @@ class Transaction:
     # t = Transaction()
     # t.add_query(q.update, grades_table, 0, *[None, 1, None, 2, None])
     """
-    def add_query(self, query, table, *args):
-        self.queries.append((query, table, args))
+    def add_query(self, query, table: Table, *args):
+        if table.name not in self.tables:
+            self.tables[table.name] = table
+
+        self.queries.append((query, table.name, args))
+
 
     def run(self):
         for query, table, args in self.queries:
             query_name = query.__name__
 
             if query_name == 'select':
-                result, holding_locks, success_rids, key_locks = query.__self__.select_transaction(
+                result, holding_locks = query.__self__.select_transaction(
                     *args, transaction_id = self.tid
                 )
             elif query_name == 'insert':
-                result, holding_locks, success_rids, key_locks = query.__self__.insert_transaction(
+                result, holding_locks, success_rids, old_index = query.__self__.insert_transaction(
                     *args, transaction_id = self.tid
                 )
-                self.success_rids += [[query_name, table, success_rids]]
+
+                if result is not False:
+                    self.success_rids += [[query_name, table, success_rids]]
+                    self.add_to_index(success_rids[0], table, old_index)
+                # Add old index here
             elif query_name == 'update':
-                result, holding_locks, success_rids, key_locks = query.__self__.update_transaction(
+                result, holding_locks, success_rids, old_index = query.__self__.update_transaction(
                     *args, transaction_id = self.tid, prevTailDict = self.update_rid
                 )
 
                 if result is not False:
                     self.update_rid[success_rids[0]] = success_rids[1]
-
-                self.success_rids += [[query_name, table, success_rids]]
+                    self.success_rids += [[query_name, table, success_rids]]
+                    self.add_to_index(success_rids[0], table, old_index)
             elif query_name == 'delete':
-                result, holding_locks, success_rids, key_locks = query.__self__.delete_transaction(
+                result, holding_locks, success_rids, old_index = query.__self__.delete_transaction(
                     *args, transaction_id = self.tid
                 )
-                self.success_rids += [(query_name, table, success_rids)]
+
+                if result is not False:
+                    self.success_rids += [(query_name, table, success_rids)]
+                    self.add_to_index(success_rids[0], table, old_index)
+            elif query_name == 'sum':
+                result, holding_locks = query.__self__.sum_transaction(
+                    *args, transaction_id = self.tid
+                )
             else:
-                result, holding_locks, success_rids, key_locks = query.__self__.sum_transaction(
+                result, holding_locks = query.__self__.increment_transaction(
                     *args, transaction_id = self.tid
                 )
 
             self.locks += [(table, holding_locks)]
-            self.key_locks += [(table, key_locks)]
-            # If the query has failed the transaction should abort
+
             if result == False:
+                # If the query has failed the transaction should abort
                 print(self.tid, query_name, args, 'failed!')
                 return self.abort()
+        # No issue after all transactions, commit!
         return self.commit()
+
+
+    def add_to_index(self, table_name, base_rid,old_index):
+            record = self.query_index.get((table_name, base_rid))
+            if record is None:
+                self.query_index[(table_name, base_rid)] = old_index
+            else:
+                for idx, column_info in enumerate(old_index):
+                    if column_info:
+                        if not record[idx]:
+                            record[idx] = column_info
+                        else:
+                            record[idx][1] = column_info[1]
+                self.query_index[(table_name, base_rid)] = record
+                       
 
     def abort(self):
         # Delete success_rids and undo index
         for query_table_rids in self.success_rids:
-            query_name = query_table_rids[0]
-            table = query_table_rids[1]
-            rids = query_table_rids[2]
+            query_name, table_name, rids = query_table_rids[0], query_table_rids[1], query_table_rids[2]
+            table = self.tables[table_name]
+
             if query_name == 'insert':
                 # set invalid
                 rid = rids[0]
@@ -103,15 +141,20 @@ class Transaction:
                     base_offset, 200000000, 0
                 )
 
+        self.undo_index()
         self.unlock_all_locks()
         return False
+
+
+    def undo_index(self):
+        pass
+
 
     def commit(self):
         # TODO: commit to database
         for query_table_rids in self.success_rids:
-            query_name = query_table_rids[0]
-            table = query_table_rids[1]
-            rids = query_table_rids[2]
+            query_name, table_name, rids = query_table_rids[0], query_table_rids[1], query_table_rids[2]
+            table = self.tables[table_name]
 
             if query_name == 'insert':
                 # set timestamp
@@ -155,14 +198,11 @@ class Transaction:
         self.unlock_all_locks()
         return True
 
+
     """
     Unlock all holding locks
     """
     def unlock_all_locks(self):
-        for table_lock in self.locks:
-            for lock in table_lock[1]:
-                table_lock[0].lock_manager.unlock(self.tid, lock)
-        for table_lock in self.key_locks:
-            for lock in table_lock[1]:
-                table_lock[0].lock_manager.unlock_key(self.tid, lock)
-        pass
+        for tablename_lock in self.locks:
+            for lock in tablename_lock[1]:
+                self.tables[tablename_lock[0]].lock_manager.unlock(self.tid, lock)
