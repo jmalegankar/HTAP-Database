31da89bc30fe4b3397cf2cc83f5880c590ef00f6
guan-kevin <github@pigzy.net>
2022-03-11 11:36:11 -0800

    Added speed test

--- tester2.py
+++ tester2.py
@@ -20,7 +20,7 @@ except:
 	
 db = Database()
 db.open('./database')
-table = db.create_table('test', 5, 0)
+table = db.create_table('test', 10, 0)
 
 transaction_workers = []
 transactions = []
@@ -31,7 +31,7 @@ for i in range(100):
 for i in range(10000):
 	q = Query(table)
 	t = transactions[i % 100]
-	t.add_query(q.insert, table, *[i * 1, i * 2, i * 3, i * 4, i * 5])
+	t.add_query(q.insert, table, *[i * 1, i * 2, i * 3, i * 4, i * 5, i * 6, i * 7, i * 8, i * 9, i * 10])
 
 for i in range(10):
 	transaction_workers.append(TransactionWorker())
@@ -54,12 +54,12 @@ print('Select after insert')
 
 q = Query(table)
 for i in range(10000):
-	result = q.select(i, 0, [1, 1, 1, 1, 1])
+	result = q.select(i, 0, [1] * 10)
 	if result is not None and len(result) > 0:
-		if result[0].columns != [i, i * 2, i * 3, i * 4, i * 5]:
-			print(i, 'is incorrect, got', result[0], 'expect', [i, i * 2, i * 3, i * 4, i * 5])
+		if result[0].columns != [i, i * 2, i * 3, i * 4, i * 5, i * 6, i * 7, i * 8, i * 9, i * 10]:
+			print(i, 'is incorrect, got', result[0], 'expect', [i, i * 2, i * 3, i * 4, i * 5, i * 6, i * 7, i * 8, i * 9, i * 10])
 	else:
-		print(i, 'is incorrect, got', result, 'expect', [i, i * 2, i * 3, i * 4, i * 5])
+		print(i, 'is incorrect, got', result, 'expect', [i, i * 2, i * 3, i * 4, i * 5, i * 6, i * 7, i * 8, i * 9, i * 10])
 
 
 print('Update')
@@ -74,7 +74,7 @@ for i in range(100):
 for i in range(10000):
 	q = Query(table)
 	t = transactions[i % 100]
-	t.add_query(q.update, table, i, *[None, -2 * i, -3 * i, -4 * i, -5 * i])
+	t.add_query(q.update, table, i, *[None, -2 * i, -3 * i, -4 * i, -5 * i, i * -6, i * -7, i * -8, i * -9, i * -10])
 
 for i in range(10):
 	transaction_workers.append(TransactionWorker())
@@ -97,12 +97,12 @@ print('Select after update')
 
 q = Query(table)
 for i in range(10000):
-	result = q.select(i, 0, [1, 1, 1, 1, 1])
+	result = q.select(i, 0, [1] * 10)
 	if result is not None and len(result) > 0:
-		if result[0].columns != [i, i * -2, i * -3, i * -4, i * -5]:
-			print(i, 'is incorrect, got', result[0], 'expect', [i, i * -2, i * -3, i * -4, i * -5])
+		if result[0].columns != [i, i * -2, i * -3, i * -4, i * -5, i * -6, i * -7, i * -8, i * -9, i * -10]:
+			print(i, 'is incorrect, got', result[0], 'expect', [i, i * -2, i * -3, i * -4, i * -5, i * -6, i * -7, i * -8, i * -9, i * -10])
 	else:
-		print(i, 'is incorrect, got', result, 'expect', [i, i * -2, i * -3, i * -4, i * -5])
+		print(i, 'is incorrect, got', result, 'expect', [i, i * -2, i * -3, i * -4, i * -5, i * -6, i * -7, i * -8, i * -9, i * -10])
 
 
 print('Test abort')
@@ -115,13 +115,13 @@ t2 = Transaction()
 
 q = Query(table)
 for i in range(10000):
-	t1.add_query(q.select, table, i, 0, [1, 1, 1, 1, 1])
+	t1.add_query(q.select, table, i, 0, [1] * 10)
 
 	if i == 5000:
-		t2.add_query(q.update, table, 0, *[None, None, None, None, 1])
+		t2.add_query(q.update, table, 0, *[None, None, None, None, 1, None, None, None, None, None])
 	else:
 		if i > 0:
-			t2.add_query(q.insert, table, *[-1 * i, 2 * i, 3 * i, 4 * i, 5 * i])
+			t2.add_query(q.insert, table, *[-1 * i, 2 * i, 3 * i, 4 * i, 5 * i, i * -6, i * -7, i * -8, i * -9, i * -10])
 
 worker1.add_transaction(t1)
 worker2.add_transaction(t2)
@@ -135,7 +135,7 @@ worker2.join()
 
 q = Query(table)
 for i in range(10000):
-	result = q.select(-1 * i, 0, [1, 1, 1, 1, 1])
+	result = q.select(-1 * i, 0, [1] * 10)
 	if result is not None and len(result) > 0:
 		print(i)
 	else:
@@ -151,13 +151,13 @@ for i in range(100):
 for i in range(10000):
 	q = Query(table)
 	t = transactions[i % 100]
-	t.add_query(q.update, table, i, *[None, 0, 0, 0, 0])
+	t.add_query(q.update, table, i, *[None, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
 for i in range(10):
 	transaction_workers.append(TransactionWorker())
 	
 for i in range(100):
-	transactions[i].add_query(q.update, table, -1, *[0, 0, 0, 0, 0])
+	transactions[i].add_query(q.update, table, -1, *[0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 	transaction_workers[i % 10].add_transaction(transactions[i])
 	
 for i in range(10):
@@ -172,11 +172,11 @@ for i in range(10):
 
 q = Query(table)
 for i in range(10000):
-	result = q.select(i, 0, [1, 1, 1, 1, 1])
+	result = q.select(i, 0, [1] * 10)
 	if result is not None and len(result) > 0:
-		if result[0].columns != [i, i * -2, i * -3, i * -4, i * -5]:
-			print(i, 'is incorrect, got', result[0], 'expect', [i, i * -2, i * -3, i * -4, i * -5])
+		if result[0].columns != [i, i * -2, i * -3, i * -4, i * -5, i * -6, i * -7, i * -8, i * -9, i * -10]:
+			print(i, 'is incorrect, got', result[0], 'expect', [i, i * -2, i * -3, i * -4, i * -5, i * -6, i * -7, i * -8, i * -9, i * -10])
 	else:
-		print(i, 'is incorrect, got', result, 'expect', [i, i * -2, i * -3, i * -4, i * -5])
+		print(i, 'is incorrect, got', result, 'expect', [i, i * -2, i * -3, i * -4, i * -5, i * -6, i * -7, i * -8, i * -9, i * -10])
 
 db.close()
